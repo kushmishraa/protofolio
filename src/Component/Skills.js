76338e3a7f8cfcc2badac837b10964fe8513edf2
kush <mishrash64@gmail.com>
2022-06-17 00:58:15 +0530

    image changes

--- src/Component/Skills.js
+++ src/Component/Skills.js
@@ -12,7 +12,7 @@ function Skills( bool) {
             experience={"2 years +"}
             projects = {" Created a local garage management system using cpp and oops concepts at college major project"}
             description = {"Started using this language as my base language for practice and development , learnt the concepts of coding and programming through this language you might be getting a question why i choose this language , i choose this language over any other language because of the oops concepts and the syntax of c++ is so familiar to me and i love to code programs in c++ but it doesnt bounded me to use other programming languages as the key for any other programming language is cpp !"}
-            image2={"protofolio/cpp2.gif"}
+            image2={"/protofolio/cpp2.gif"}
             show={bool.show}
           
           />
@@ -32,7 +32,7 @@ function Skills( bool) {
               experience={"2 years +"}
               projects = {"Created 2-3 projects for college main projects that used concepts of MySql and databases "}
               description = {"Started learning java as an another language that taken my intrests , I basically used java for solving online coding questions on hackerrank and others"}
-              image2={"protofolio/sql.gif"}
+              image2={"/protofolio/sql.gif"}
               show={bool.show}
 
               />
@@ -42,7 +42,7 @@ function Skills( bool) {
                 experience={"1 years "}
                 projects = {"Created ticket counter system for college project "}
                 description = {"As per current development trends all the companies requires data structres and algorithm for its core concept and to judge a candidates problem solving capabilites ,Im currently having 3* at hackerrank ."}
-                image2={"protofolio/dsa.gif"}
+                image2={"/protofolio/dsa.gif"}
                 show={bool.show}
 
 />
